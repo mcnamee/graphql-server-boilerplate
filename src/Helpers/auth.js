3c5f2c00c007812059607e3fab810b99ecc2a153
Matt Mcnamee <matt@mcnamee.co>
2019-03-15 09:54:37 +1000

    Fix for Lambda auth via headers

--- src/Helpers/auth.js
+++ src/Helpers/auth.js
@@ -14,7 +14,18 @@ class AuthError extends Error {
  * @param {*} context
  */
 function getUserId(context) {
-  const Authorization = context.request.get('Authorization');
+  let Authorization = null;
+
+  // Lambda Auth header
+  if (context && context.event && context.event.headers && context.event.headers.Authorization) {
+    Authorization = context.event.headers.Authorization; // eslint-disable-line
+  }
+
+  // Local server Auth Header
+  if (context && context.request && context.request.get('Authorization')) {
+    Authorization = context.request.get('Authorization');
+  }
+
   if (Authorization) {
     const token = Authorization.replace('Bearer ', '');
     const { userId } = jwt.verify(token, process.env.APP_SECRET);
