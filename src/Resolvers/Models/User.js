7d3f82897c0fd2ee43ed56c61f073d18174cb400
Matt Mcnamee <matt@mcnamee.co>
2019-03-18 14:18:47 +1000

    Add `name` field to users

--- src/Resolvers/Models/User.js
+++ src/Resolvers/Models/User.js
@@ -1,5 +1,10 @@
 module.exports.User = {
   /**
+   * `Combine firstname & lastname` to create a name field
+   */
+  name: ({ firstName, lastName }) => `${firstName} ${lastName}`,
+
+  /**
    * Fill the `Posts array of IDs` with the respective Posts
    */
   posts: ({ id }, args, context) => context.prisma.user({ id }).posts(),
